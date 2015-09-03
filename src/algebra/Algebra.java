8a41fb489105ee9f809b8d34388740431b4d4fdb
Aiden Sweezey <Aiden@Aidens-MacBook-Air.local>
2015-09-03 15:06:29 -0600

    Initial Commit

---
+++ src/algebra/Algebra.java
@@ -0,0 +1,21 @@
+package algebra;
+
+public class Algebra {
+	 public static void main(String[] args){
+		 isPrime(677);
+	 }
+	 
+	 public static void isPrime(int i){
+		 boolean flag = false;
+		 for(int j = 2; j < Math.sqrt(i); j++){
+			 if(i % j == 0){
+				 System.out.println(i + " is not prime. It's two factors are: " + i/j + " and " + j);
+				 flag = true;
+				 break;
+			 }
+		 }
+		 if(!flag){
+			 System.out.println(i + " is a prime number");
+		 }
+	 }
+}
